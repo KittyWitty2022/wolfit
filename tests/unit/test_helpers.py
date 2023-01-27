3ec2279c7c3fe4153360869895066f0ff4ceae80
Emergency Food <102189725+KittyWitty2022@users.noreply.github.com>
2023-01-26 23:59:02 -0800

    test_seconds complete

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -4,4 +4,6 @@ from datetime import datetime, timedelta
 def test_now():
     assert (pretty_date(datetime.utcnow())) == "just now"
 
+def test_seconds():
+    assert (pretty_date(datetime.utcnow() - timedelta(seconds=34))) == "34 seconds ago"
 
